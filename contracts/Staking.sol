fadee95c72edbdc107413e15c0a1a8577922bfb7
LG <lefgiak@gmail.com>
2023-03-25 17:15:30 +0000

    comments 1

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,7 +1,99 @@
 // SPDX-License-Identifier: Unlicense
 
 pragma solidity 0.8.17;
-import "./USDT.sol";
+
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+interface IBEP20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the token decimals.
+     */
+    function decimals() external view returns (uint8);
+
+    /**
+     * @dev Returns the token symbol.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the token name.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the bep token owner.
+     */
+    function getOwner() external view returns (address);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address _owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
 interface myNFT {
     function balanceOf(address) external view returns (uint256);
@@ -125,14 +217,6 @@ contract Staking is Ownable {
     }
 
     /**
-     * @notice function to change USDT address.
-     */
-    function changeToken(address _USDTaddress) external onlyOwner {
-        require(_USDTaddress != address(0));
-        USDT = IBEP20(_USDTaddress);
-    }
-
-    /**
      * @notice function to intiate a deposit.
      * @param _amount: amount of USDT to deposit
      */
@@ -147,29 +231,7 @@ contract Staking is Ownable {
             UsersInfo.push(msg.sender);
             user.initialDeposit += _amount - depositFee;
             user.WithdrawAddress = msg.sender;
-            address myId;
-            address UserNFTaddress = NFTContract.balanceOf(msg.sender) != 0
-                ? NFTaddress
-                : NFTaddress2;
-
-            for (uint256 i; i < 5000; ) {
-                myId = myNFT(UserNFTaddress).ownerOf(i);
-
-                if (myId == msg.sender) {
-                    user.NFTId = i;
-                    i = 5000;
-                }
-                unchecked {
-                    ++i;
-                }
             }
-        }
-
-        require(
-            myNFT(NFTaddress).ownerOf(user.NFTId) == msg.sender ||
-                myNFT(NFTaddress2).ownerOf(user.NFTId) == msg.sender,
-            "wrong NFT"
-        );
 
         user.deposits.push(
             Depo({
@@ -401,14 +463,6 @@ contract Staking is Ownable {
         return (finalAmount, fee);
     }
 
-    /**
-     * @notice function to change drop rate per day.
-     * @param _newRate: rate 6 is 0.6% per day
-     */
-    function changeDROP_RATE(uint256 _newRate) external onlyOwner {
-        DROP_RATE = _newRate;
-    }
-
     function checkReq(
         uint256 amount,
         uint256 time,
@@ -419,7 +473,7 @@ contract Staking is Ownable {
         accepted = (amount > 0 &&
             ((block.timestamp > time + 28 days &&
                 block.timestamp < time + 60 days) ||
-                (block.timestamp > time + 60 days && unlocked == 1)) &&
+                (block.timestamp > time + 60 days && unlocked !=0)) &&
             // can only do once per 2 weeks
             block.timestamp - lastActiontime > 7 days);
     }
